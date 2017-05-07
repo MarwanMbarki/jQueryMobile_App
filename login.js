67e8d9633e4be18dda80ba020eaa3633e8659a42
Marwan Mbarki <MarwanMbarki@users.noreply.github.com>
2017-05-08 00:27:58 +0100

    Add files via upload

---
+++ login.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    $("#login").click(function () {
+        email = $("#email").val();
+        password = $("#password").val();
+        $.ajax({
+            type: "POST",
+            url: "get-data.php",
+            data: "email=" + email + "&password=" + password,
+            success: function (html) {
+                if (html == 'true') {
+                    window.location = "dashboard.php";
+                }
+                else {
+                    $("#add_err").css('display', 'inline', 'important');
+                    $("#add_err").html("<img src='images/alert.png' />Wrong username or password");
+                }
+            },
+            beforeSend: function () {
+                $("#add_err").css('display', 'inline', 'important');
+                $("#add_err").html("<img src='images/ajax-loader.gif' /> Loading...")
+            }
+        });
+        return false;
+    });
+});
