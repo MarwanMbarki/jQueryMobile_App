a77a579803f2d7108b69c0eaf3b089efb5230d0c
Marwan Mbarki <MarwanMbarki@users.noreply.github.com>
2017-05-09 11:33:30 +0100

    Add files via upload

---
+++ get-data.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+
+$con = mysql_connect('localhost', 'root', '', 'jQuery') or die(mysql_error());
+
+if (!$con) {
+    echo "Unable to connect to DB: " . mysql_error();
+    exit;
+}
+
+if (!mysql_select_db("DATABASENAME")) {
+    echo "Unable to select mydbname: " . mysql_error();
+    exit;
+}
+$email = $_POST['email'];
+$pWord = md5($_POST['password']);
+$qry = "SELECT usrid, email, oauth FROM users WHERE email='" . $email . "' AND password='" . $pWord . "' AND status='active'";
+$res = mysql_query($qry);
+$num_row = mysql_num_rows($res);
+$row = mysql_fetch_assoc($res);
+if ($num_row == 1) {
+    echo 'true';
+    $_SESSION['email'] = $row['email'];
+    $_SESSION['oId'] = $row['orgid'];
+    $_SESSION['auth'] = $row['oauth'];
+} else {
+    echo 'false';
+}
+?>
