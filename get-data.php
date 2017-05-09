de0f8662ee5050d5fd040db640bc3d5e06447ab4
Marwan Mbarki <MarwanMbarki@users.noreply.github.com>
2017-05-10 00:53:51 +0100

    Update get-data.php

--- get-data.php
+++ get-data.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 //Database Connection
-$con = mysql_connect('localhost', 'root', '', 'jQuery') or die(mysql_error());
+$con = mysqli_connect('localhost', 'root', '', 'jQuery') or die(mysqli_error());
 
 if (!$con) {
-    echo "Unable to connect to DB: " . mysql_error();
+    echo "Unable to connect to DB: " . mysqli_error();
     exit;
 }
 
@@ -16,9 +16,9 @@ if (!mysql_select_db("DATABASENAME")) {
 $email = $_POST['email'];
 $pWord = md5($_POST['password']);
 $qry = "SELECT usrid, email, oauth FROM users WHERE email='" . $email . "' AND password='" . $pWord . "' AND status='active'";
-$res = mysql_query($qry);
-$num_row = mysql_num_rows($res);
-$row = mysql_fetch_assoc($res);
+$res = mysqli_query($qry);
+$num_row = mysqli_num_rows($res);
+$row = mysqli_fetch_assoc($res);
 if ($num_row == 1) {
     echo 'true';
     $_SESSION['email'] = $row['email'];
