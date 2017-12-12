bb39f9d81935ed1b7b602bb62a7c616c11175e69
Marwan Mbarki <MarwanMbarki@users.noreply.github.com>
2017-12-12 22:27:08 +0000

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ insert_film();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Title -->
-    <title>Weekly Blog/title>
+    <title>Blog/title>
     <!-- Page Description -->
     <!-- jQuerry stylesheet>  -->
     <script src="js/jquery.min.js"></script>
