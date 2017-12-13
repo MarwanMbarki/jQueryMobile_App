663d7a330407c751076d0ac115f3196c57571107
Marwan Mbarki <MarwanMbarki@users.noreply.github.com>
2017-12-13 11:38:02 +0000

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,6 @@ insert_film();
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Title -->
     <title>Blog/title>
-    <!-- Page Description -->
     <!-- jQuerry stylesheet>  -->
     <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.js"></script>
